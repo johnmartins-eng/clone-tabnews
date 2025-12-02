3f03eb966ca6995c8c4414a1bbe2ea5b160dc95c
John Martins <johndodo07@gmail.com>
2025-12-02 00:43:50 -0300

    adiciona scripts de `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Implementação do zero do https://www.tabnews.com.br/ em conjunto com curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-function Home(){
-  return <h1>Gabriela eu te amo, se você tambem me ama dá uma risadinha!!!</h1>
+function Home() {
+  return <h1>Gabriela eu te amo, se você tambem me ama dá uma risadinha!!!</h1>;
 }
 
 export default Home;
